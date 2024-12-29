24f13bba2d1318e8a207b22952c4b48cdd33971a
Ado Pabianko <adopabianko@gmail.com>
2024-12-29 15:39:27 +0700

    refactor: move payment table to payment service

--- services/payment/db/migrations/000001_create_payment_table.up.sql
+++ services/payment/db/migrations/000001_create_payment_table.up.sql
@@ -1,6 +1,6 @@
-CREATE TABLE IF NOT EXISTS order_payments(
+CREATE TABLE IF NOT EXISTS payments(
     id SERIAL PRIMARY KEY NOT NULL,
-    order_id SERIAL NOT NULL,
+    order_code VARCHAR(50) NOT NULL,
     payment_type VARCHAR(50) NOT NULL,
     amount NUMERIC(10,2) NOT NULL,
     status VARCHAR(50) NOT NULL,
@@ -8,7 +8,5 @@ CREATE TABLE IF NOT EXISTS order_payments(
     created_at TIMESTAMP NOT NULL DEFAULT NOW(),
     created_by VARCHAR(100) NOT NULL,
     deleted_at TIMESTAMP,
-    deleted_by VARCHAR(100),
-
-    CONSTRAINT fk_order_id FOREIGN KEY(order_id) REFERENCES "orders"(id)
+    deleted_by VARCHAR(100)
 );
