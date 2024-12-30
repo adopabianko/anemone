edfbff38bcb1bd74545313b3a42794c8ebd8b43a
Ado Pabianko <adopabianko@gmail.com>
2024-12-30 12:43:49 +0700

    refactor: rename table order_products to order_items

--- services/order/db/migrations/000002_create_order_items_table.up.sql
+++ services/order/db/migrations/000002_create_order_items_table.up.sql
@@ -1,8 +1,8 @@
-CREATE TABLE IF NOT EXISTS order_products(
+CREATE TABLE IF NOT EXISTS order_items(
     id SERIAL PRIMARY KEY NOT NULL,
     order_id SERIAL NOT NULL,
-    product_id SERIAL NOT NULL,
-    product_pricing_id SERIAL NOT NULL,
+    product_sku  VARCHAR(50) NOT NULL,
+    product_name VARCHAR(100) NOT NULL,
     sku VARCHAR(50) NOT NULL,
     product_name VARCHAR(100) NOT NULL,
     price NUMERIC(10,2) NOT NULL,
@@ -11,5 +11,5 @@ CREATE TABLE IF NOT EXISTS order_products(
     created_at TIMESTAMP NOT NULL DEFAULT NOW(),
     created_by VARCHAR(100) NOT NULL,
     deleted_at TIMESTAMP,
-    deleted_by VARCHAR(100)
+    deleted_by VARCHAR(100),
 );
