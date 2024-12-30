876c19c1a8b8a0a7529d673f1179dc3cdbc6d0e9
Ado Pabianko <adopabianko@gmail.com>
2024-12-30 22:24:15 +0700

    fix: migrations order items

--- services/order/db/migrations/000002_create_order_items_table.up.sql
+++ services/order/db/migrations/000002_create_order_items_table.up.sql
@@ -1,9 +1,7 @@
 CREATE TABLE IF NOT EXISTS order_items(
     id SERIAL PRIMARY KEY NOT NULL,
     order_id SERIAL NOT NULL,
-    product_sku  VARCHAR(50) NOT NULL,
-    product_name VARCHAR(100) NOT NULL,
-    sku VARCHAR(50) NOT NULL,
+    product_sku VARCHAR(50) NOT NULL,
     product_name VARCHAR(100) NOT NULL,
     price NUMERIC(10,2) NOT NULL,
     quantity NUMERIC(10,2) NOT NULL,
@@ -11,5 +9,5 @@ CREATE TABLE IF NOT EXISTS order_items(
     created_at TIMESTAMP NOT NULL DEFAULT NOW(),
     created_by VARCHAR(100) NOT NULL,
     deleted_at TIMESTAMP,
-    deleted_by VARCHAR(100),
+    deleted_by VARCHAR(100)
 );
